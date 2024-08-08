70484aafdfb1a15744e152fa5eefab70d393c2d6
Tshelthrim <tshelthrimchoden678@gmail.com>
2024-08-08 14:49:52 +0800

    ??

--- index.js
+++ index.js
@@ -32,10 +32,6 @@ let data = [
     }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-
 app.get('/api/persons',(request,response)=>{
     response.json(data)
 })
